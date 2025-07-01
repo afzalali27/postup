6d76294cae8269bcca0573979f32d1954712de31
Afzal Ali <afzalali2728@gmail.com>
2025-07-02 00:09:31 +0500

    fetch and display posts from public API

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, ScrollView, ActivityIndicator, Text, StyleSheet } from 'react-native';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import PostItem from '../components/PostItem';
+import { fetchPosts } from '../services/api';
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 const HomeScreen = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+        const data = await fetchPosts();
+        setPosts(data);
+    };
+
+    loadData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Header />
       <ScrollView contentContainerStyle={styles.scrollContent}>
-        {/* Posts will be added here later */}
+        {posts.map((post) => (
+            <PostItem key={post.id} {...post} />
+          ))}
       </ScrollView>
       <Footer />
     </View>
